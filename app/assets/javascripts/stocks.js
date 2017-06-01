6d42fd6aad8d2516660c7aca845a4eba4a580db6
agam98 <dwivedi.agam233@gmail.com>
2017-06-01 18:30:26 +0000

    Added Spinner using AJAX and jQuery

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,12 +1,20 @@
 var init_stock_lookup;
 
 init_stock_lookup = function(){
+  $('#stock-lookup-form').on('ajax:before', function(event , data, status){
+    show_spinner();
+    
+  });
+  $('#stock-lookup-form').on('ajax:after', function(event , data, status){
+    hide_spinner();
+  });
   $('#stock-lookup-form').on('ajax:success', function(event, data, status){
     $('#stock-lookup').replaceWith(data);
     init_stock_lookup();
   });
 
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, data, error){
+    hide_spinner();
     $('#stock-lookup-results').replaceWith(' ');
     $('#stock-lookup-errors').replaceWith('Stock was not found.');
   });
