867606e596eb7f220aa36358567a43e0e52fd7cf
agam98 <dwivedi.agam233@gmail.com>
2017-06-08 08:54:42 +0000

    Stock Restrictions

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -42,7 +42,7 @@ class UserStocksController < ApplicationController
     end
     respond_to do |format|
       if @user_stock.save
-        format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.stock.ticker} stock was successfully added!" }
+        format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.stock.ticker} was successfully added!" }
         format.json { render :show, status: :created, location: @user_stock }
       else
         format.html { render :new }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
+  
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+  
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
 end
