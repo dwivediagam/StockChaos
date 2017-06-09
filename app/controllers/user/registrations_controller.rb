98e651465c14d9272b77826ab762ccd069a73f49
agam98 <dwivedi.agam233@gmail.com>
2017-06-09 20:12:26 +0000

    Removed small bug

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -5,7 +5,7 @@ class User::RegistrationsController < Devise::RegistrationsController
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
-    devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
