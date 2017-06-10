d41ba75f49ce73ff10bae08083387d7db8743009
agam98 <dwivedi.agam233@gmail.com>
2017-06-10 08:53:28 +0000

    Updated Friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609184029) do
+ActiveRecord::Schema.define(version: 20170610082556) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
