fcb4e2c4ff9a4a5075fd52b705b22f45a5bd6e76
agam98 <dwivedi.agam233@gmail.com>
2017-06-11 04:57:11 +0000

    Added friend and search friends functionality using AJAX

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,26 @@ class UsersController < ApplicationController
     @friendships = current_user.friends
   end
   
+  def search
+    @users = User.search(params[:search_param])
+    
+    if @users
+      @users = current_user.except_current_user(@users)
+      render partial: "friends/lookup"
+    else
+      render status: :not_found, nothing: true
+    end
+  end
+  
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    
+    if current_user.save
+      redirect_to my_friends_path, notice: "Friend was successfully added."
+    else
+      redirect_to my_friends_path, flash[:error] = "There was an error with adding user as a friend"
+    end
+  end
+  
 end
